ce404508cc217377d6d1f832a5bd5a5a04b0ab60
cksdnr3 <cksdnr3333@naver.com>
2021-03-14 22:55:48 +0900

    21.03.14

--- fe/src/index.js
+++ fe/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import UserDetail from './uss/component/UserDetail'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <UserDetail />
   </React.StrictMode>,
   document.getElementById('root')
 );
